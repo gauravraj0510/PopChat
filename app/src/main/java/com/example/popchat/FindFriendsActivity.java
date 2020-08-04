043f4501a1e180c986009bb6db22c47b9c8abfcd
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-08-04 18:31:58 +0530

    search bar added in FindFriendsActivity

--- app/src/main/java/com/example/popchat/FindFriendsActivity.java
+++ app/src/main/java/com/example/popchat/FindFriendsActivity.java
@@ -11,12 +11,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -26,6 +29,8 @@ public class FindFriendsActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private RecyclerView findFriendsRecyclerView;
     private DatabaseReference usersRef;
+    private Button findUserButton;
+    private EditText searchUserField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +41,66 @@ public class FindFriendsActivity extends AppCompatActivity {
 
         findFriendsRecyclerView = findViewById(R.id.find_friends_recycler_view);
         mToolbar = findViewById(R.id.find_friends_toolbar);
+        searchUserField = findViewById(R.id.search_users_input_field);
+        findUserButton = findViewById(R.id.find_users_button);
 
         findFriendsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
+
+        findUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String searchText = searchUserField.getText().toString();
+                firebaseUserSearch(searchText);
+            }
+        });
     }
+
+    private void firebaseUserSearch(String searchText) {
+
+        Query firebaseSearchQuery = usersRef.orderByChild("name").startAt(searchText).endAt(searchText+"\uf8ff");
+
+        FirebaseRecyclerOptions<ContactsAdapter> options = new FirebaseRecyclerOptions.Builder<ContactsAdapter>()
+                .setQuery(firebaseSearchQuery,ContactsAdapter.class)
+                .build();
+
+        FirebaseRecyclerAdapter<ContactsAdapter, FindFriendViewHolder> adapter = new FirebaseRecyclerAdapter<ContactsAdapter, FindFriendViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, final int position, @NonNull ContactsAdapter model) {
+
+                holder.userName.setText(model.getName());
+                holder.userStatus.setText(model.getStatus());
+                Picasso.get().load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String visitUserId = getRef(position).getKey();
+                        Intent profileIntent = new Intent(FindFriendsActivity.this, ProfileActivity.class);
+                        profileIntent.putExtra("visitUserId", visitUserId);
+                        startActivity(profileIntent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent,false);
+                FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                return viewHolder;
+            }
+        };
+
+        findFriendsRecyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
 
     @Override
     protected void onStart() {
