16f67f13b70cce7805c01cc3d48d751ca79704ff
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-08-07 11:28:17 +0530

    InstructionActivity typo fixed and added hyperlink

--- app/src/main/java/com/example/popchat/InstructionActivity.java
+++ app/src/main/java/com/example/popchat/InstructionActivity.java
@@ -3,19 +3,46 @@ package com.example.popchat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 
 public class InstructionActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TextView instructions, howTo;
+    private TextView githubLink, githubRepoLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        githubLink = findViewById(R.id.tv_github_profile_link);
+        githubRepoLink = findViewById(R.id.tv_github_repo_link);
+
+        githubLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/gauravraj0510";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        githubRepoLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/gauravraj0510/PopChat";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
         mToolbar = findViewById(R.id.instruction_toolbar);
         instructions = findViewById(R.id.textView_instructions);
